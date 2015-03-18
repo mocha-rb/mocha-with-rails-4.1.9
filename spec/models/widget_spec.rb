ba29ea33e4663f55328bf6a9efc358e60b022ec8
James Mead <james@floehopper.org>
2015-03-18 15:19:35 +0000

    Add Mocha-related spec.

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'foo' do
+    mock = mock("mock")
+    mock.expects(:foo).with(123)
+  end
 end
